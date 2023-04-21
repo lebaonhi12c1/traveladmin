a742e2e22f55bc3e2e35e08b992fcb5e9eb10313
phamminhquan12c1 <phamminhquan12c1@gmail.com>
2023-04-21 22:43:49 +0700

    update admin 21/4 ver2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {AiOutlinePoweroff} from 'react-icons/ai'
 function Navbar(props) {
-    const data = JSON.parse(localStorage.getItem("user")).name
-    console.log(data)
+    
+    // console.log(data)
     return (
         <div className='fixed  top-0 right-0 bg-white shadow-lg left-[250px] h-[60px] z-50'>
             <div className='flex items-center gap-2 justify-end h-full px-10'>
                <img src="https://img.freepik.com/free-photo/portrait-pretty-korean-girl-receive-surprising-news-looking-amazed-happy-camera-standing-blue-background_1258-76005.jpg?w=996&t=st=1681209515~exp=1681210115~hmac=d23be51da30d0c60529245ec94173434ae946d4e0a550ef2fc7a13549b3ec5ae" alt="" className='w-10 h-10 rounded-full'/>
-               <div>{data}</div>
+               <div>{JSON.parse(localStorage.getItem("user"))?.name ?? 'adminname'}</div>
                <div className='border border-slate-400 h-[40%]'></div>
                <div><AiOutlinePoweroff className='text-[20px] text-red-600 cursor-pointer select-none' onClick={()=>{localStorage.removeItem("user");window.location.reload(true)}} title='Logout'/></div>
             </div>
